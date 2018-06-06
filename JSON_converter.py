c11bdb1d7ab135e72b63ae594f94baa4db5689d3
vovet12 <www.vova121998@gmail.com>
2018-06-07 00:46:45 +0300

    TASK3: lists added

--- JSON_converter.py
+++ JSON_converter.py
@@ -7,12 +7,16 @@ HTML_FILE = "source.html"
 def convert_file():
     try:
         html_content = ""
-        with open(JSON_FILE, 'r') as f:
+        with open(JSON_FILE) as f:
             jdata = json.load(f)
+        html_content += "<ul>"
         for json_dict in jdata:
+            html_dict = "<li>"
             for key, value in json_dict.items():
-                html_content += '<{0}>{1}</{0}>'.format(key,
-                                                        value)
+                html_dict += '<{0}>{1}</{0}>'.format(key,
+                                                    value)
+            html_content += html_dict + "</li>"
+        html_content += "</ul>"
         with open(HTML_FILE, 'w') as f:
             f.write(html_content)
         print('Done!')
